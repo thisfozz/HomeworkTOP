07a1e21ef2031da8002b4767668bd028084e70ac
kfcfozz@vk.com <kfcfozz@vk.com>
2023-03-02 10:46:28 +0500

    Теперь везде, где надо возвращаю значения

--- HomeworkTOP/HomeworkTOP.cpp
+++ HomeworkTOP/HomeworkTOP.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int m_pow(const int& value, const int& degree) {
-    int result = 1;
+    int result{1};
     for (int i = 0; i < degree; ++i) {
         result *= value;
     }
@@ -11,16 +11,17 @@ int m_pow(const int& value, const int& degree) {
     return result;
 }
 
-void range(const int& valueOne, const int& valueTwo) {
-    int result = 0;
+int range(const int& valueOne, const int& valueTwo) {
+    int result{1};
     for (int i = valueOne; i <= valueTwo; ++i) {
         result += i;
     }
-    std::cout << result;
+
+    return result;
 }
 
 bool isPerfect(const int& value) {
-    int sum = 0;
+    int sum{1};
     for (int i = 1; i < value; i++) {
         if (value % i == 0)
             sum += i;
@@ -34,8 +35,8 @@ bool isPerfect(const int& value) {
 }
 
 int isPerfect(const int& begin, const int& end) {
-    bool isPerfectCheck = false;
-    int isPerfectValue = 0;
+    bool isPerfectCheck{false};
+    int isPerfectValue{1};
     for (int i = begin; i < end; ++i) {
         if (isPerfect(i)) {
             isPerfectCheck = true;
@@ -56,7 +57,7 @@ void card(const char& card, const int& value) {
 
 int print(const std::string& s)
 {
-    int result = 0;
+    int result{0};
     for (std::string::size_type i = 0; i <= s.size(); ++i) {
         result += (int)i;
     }
