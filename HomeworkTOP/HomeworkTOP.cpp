fbc238eb1eb0cf518bba281609bb9559e7e5b4ff
kfcfozz@vk.com <kfcfozz@vk.com>
2023-03-04 12:03:52 +0500

    Домашнее задание

--- HomeworkTOP/HomeworkTOP.cpp
+++ HomeworkTOP/HomeworkTOP.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
 #include <math.h>
 #include <string>
+#include "HomeworkTOP2.cpp"
 
 int m_pow(const int& value, const int& degree) {
-    int result{1};
+    int result{ 1 };
     for (int i = 0; i < degree; ++i) {
         result *= value;
     }
@@ -12,7 +13,7 @@ int m_pow(const int& value, const int& degree) {
 }
 
 int range(const int& valueOne, const int& valueTwo) {
-    int result{1};
+    int result{ 1 };
     for (int i = valueOne; i <= valueTwo; ++i) {
         result += i;
     }
@@ -21,7 +22,7 @@ int range(const int& valueOne, const int& valueTwo) {
 }
 
 bool isPerfect(const int& value) {
-    int sum{1};
+    int sum{ 1 };
     for (int i = 1; i < value; i++) {
         if (value % i == 0)
             sum += i;
@@ -35,8 +36,8 @@ bool isPerfect(const int& value) {
 }
 
 int isPerfect(const int& begin, const int& end) {
-    bool isPerfectCheck{false};
-    int isPerfectValue{1};
+    bool isPerfectCheck{ false };
+    int isPerfectValue{ 1 };
     for (int i = begin; i < end; ++i) {
         if (isPerfect(i)) {
             isPerfectCheck = true;
@@ -57,7 +58,7 @@ void card(const char& card, const int& value) {
 
 int print(const std::string& s)
 {
-    int result{0};
+    int result{ 0 };
     for (std::string::size_type i = 0; i <= s.size(); ++i) {
         result += (int)i;
     }
@@ -65,8 +66,101 @@ int print(const std::string& s)
     return result;
 }
 
-int main()
-{
+void fillArrayRand(int* array, const int& SIZE) {
+    srand(time(NULL));
 
+    for (size_t i = 0; i < SIZE; ++i) {
+        array[i] = 1 + rand() % 100;
+    }
 }
 
+void fillArrayLinear(int* array, const int& SIZE) {
+    for (size_t i = 0; i < SIZE; ++i) {
+        array[i] = i + 1;
+    }
+}
+
+int linearSearch(int* array, const int& SIZE, const int& value) {
+    for (size_t i = 0; i < SIZE; ++i) {
+        if (array[i] == value) {
+            return i;
+        }
+    }
+}
+
+//PERFORMED MENTALLY
+int binarySearch(int* array, int min, int max) {
+
+    if (max < min)
+        std::cout << "MAX < MIN";
+    int mid = (min + max) / 2;
+    char choice{};
+
+    std::cout << "Your value: " << " > " " OR " << " < " << mid << " ? ";
+    std::cout << "\nY - YES   :   N - NO   :   =   ";
+    std::cin >> choice;
+    switch (choice)
+    {
+    case '<':
+            return binarySearch(array, min, mid - 1);
+        break;
+    case '>':
+            return binarySearch(array, mid + 1, max);
+        break;
+    case '=':
+        std::cout << "Value is find: " << mid;
+        return 1;
+
+        break;
+    default:
+        break;
+    }
+}
+
+int main()
+{
+    const int SIZE = 100;
+    int array[SIZE]{};
+    int choice{ 0 }, value{}, begin{}, end{};
+
+    std::cout
+        << "1. Linear Search. "
+        << "\n2. Binary Search. ";
+
+    std::cout << "\nEnter choice: ";
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        system("cls");
+
+        std::cout << "Linear Search. ";
+
+        std::cout << "Enter value for search: ";
+        std::cin >> value;
+
+        system("cls");
+
+        fillArrayRand(array, SIZE);
+        std::cout << "Value: " << value << "Is find on key: " << linearSearch(array, SIZE, value);
+        break;
+    case 2:
+        system("cls");
+
+        std::cout << "Binary Search. ";
+        std::cout << "\nMake a number in your mind.";
+
+        std::cout << "\nEnter range: ";
+        std::cout << "\nBegin = "; std::cin >> begin;
+        std::cout << "\nEnd = "; std::cin >> end;
+
+        system("cls");
+
+        fillArrayLinear(array, SIZE);
+        binarySearch(array, begin, end);
+        break;
+    default:
+        break;
+    }
+}
